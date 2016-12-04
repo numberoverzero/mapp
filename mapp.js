677ac93de4fc67a097c2526c190f61086fd13008
Joe Cross <joe.mcross@gmail.com>
2016-12-04 01:41:43 -0800

    Restructured entire mapp module.  No significant functional changes

--- mapp.js
+++ mapp.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /*
  *  routes in _dynamicRoutes.json can include a map of regex -> fixed url, such as:
  *    {
@@ -24,125 +22,93 @@
  *    └── login.html       }
  */
 
-(function (exports) {
-    const _mapp = {};
-    const pageCache = {};
-    const rewrites = {};
-    let dynamicRoutes;
-    let sameOrigin;
+mapp = (() => {
+    "use strict";
+    let
+        // set externally
+        container = null,
 
-    // set by the caller
-    _mapp.container = null;
+        // loaded from localStorage or /_dynamicRoutes.json
+        dynamicRoutes,
 
-    // hold a reference to the promise's resolve cb to manually complete
-    let onReady;
-    _mapp.ready = new Promise(resolve=> onReady=resolve);
+        // hold references to the promise's callbacks to manually complete
+        onReady, onNotReady,
+        ready = new Promise((resolve, reject)=> {onReady=resolve; onNotReady=reject})
+    ;
+    const
+        origin = location.origin,
+        partials = "_",
+        dynamicRoutesUrl = "_dynamicRoutes.json",
+
+        // strip out origin, partials, and leading slashes
+        partialRegex = new RegExp(
+            "/?" + "^(?:" + origin + ")?" +
+            "/?" + "(?:" + partials + ")?"+
+            "/?" + "(.*)$" // this is what we care about
+        ),
+
+        // coerce eg. document.location to string
+        U = url => "" + (url || ""),
+
+        // only rewrite local urls
+        sameOrigin = url => U(url).indexOf(origin) === 0
+    ;
 
 
-    const origin = location.origin,
-          root = "",
-          partials = "/_",
-          dynamicRoutesUrl = "_dynamicRoutes.json";
-
-    // strips out origin, root, partials directory, and leading slash
-    const partialRegex = new RegExp(
-          "^(?:" + origin + ")?" +
-          "(?:" + root + ")?" +
-          "(?:" + partials + ")?"+
-          "/?" + // always drop leading slash
-          "(.*)$" // this is what we care about
-      );
-
-    // helper to ensure url is a string
-    const U = url => "" + (url || "");
-
-    const dynamicRouteFor = function (url) {
+    // =========
+    //  Routing
+    // =========
+    function dynamicRouteFor (url) {
+        // always call with U(url)
         if (!dynamicRoutes) return null;
-        url = U(url).replace(partialRegex, "$1");
-        const len = dynamicRoutes.length;
-        for (let route, match, index = 0; index < len; index++) {
+        url = url.replace(partialRegex, "$1");
+        for (let route, match, index = 0; index < dynamicRoutes.length; index++) {
             route = dynamicRoutes[index];
             if (match = url.match(route.pattern))
                 return {pattern: route.pattern, to: route.to, match: match};
         }
         return null;
-    };
+    }
 
-    const partialOf = function (url, dynamic) {
+    function urlFragment (url, dynamic) {
         url = U(url).replace(partialRegex, "$1");
-        if (!dynamic || !dynamicRoutes) return url;
+        if (!dynamic) return url;
         const route = dynamicRouteFor(url);
         return route? route.to : url;
-    };
+    }
 
-    rewrites.cache = url=>partialOf(url, true);
-    // http://jsben.ch/#/1o8xK faster than [x, y, z].join("")
-    rewrites.partialUrl = url=>origin + root + partials + "/" + partialOf(url, true);
-    rewrites.displayUrl = url=>origin + root + "/" + partialOf(url, false);
-
-    sameOrigin = url => U(url).indexOf(origin) === 0;
-
-    _mapp.match = url => {
-        // most uses are matching the current url anyway
-        url = url || document.location;
-        const route = dynamicRouteFor(url);
-        return route ? route.match : null;
-    };
+    const
+        cacheKey = url=>urlFragment(url, true),
+        urlOfPartial = url=>origin + "/" + partials + "/" + urlFragment(url, true),
+        urlForDisplay = url=>origin + "/" + urlFragment(url, false);
 
 
-    // Load dynamic rewrites from localStorage, fall back to url
-    // ---------------------------------------------------------
-
+    // on init load dynamicRoutes
+    // --------------------------
     function compileRoutes(routes) {
-        dynamicRoutes = [];
-        Object.keys(routes).forEach(pattern => {
-            dynamicRoutes.push({
-                pattern: new RegExp(pattern),
-                to: routes[pattern]
-            });
-        });
+        dynamicRoutes = Object.keys(routes).map(
+            pattern => ({pattern: new RegExp(pattern), to: routes[pattern]})
+        ); onReady();
     }
 
-    const serializedLocalRoutes = localStorage.getItem("_dynamicRoutes");
-    if (serializedLocalRoutes) {
-        compileRoutes(JSON.parse(serializedLocalRoutes));
-        onReady();
-    } else {
-        rq(rewrites.displayUrl(dynamicRoutesUrl), {r:"json"})
+    const localRoutes = localStorage.getItem("_dynamicRoutes");
+    if (localRoutes) compileRoutes(JSON.parse(localRoutes));
+    else {
+        rq(urlForDisplay(dynamicRoutesUrl), {r:"json"})
         .then(xhr => {
-            if (xhr.status != 200) return;
-            const routes = xhr.response;
-            localStorage.setItem("_dynamicRoutes", JSON.stringify(routes));
-            compileRoutes(routes);
-        })
-        .then(onReady)
-        .catch(onReady);  // call failed, tried enough to be "ready"
+            if (xhr.status == 200) {
+                localStorage.setItem("_dynamicRoutes", JSON.stringify(xhr.response));
+                compileRoutes(xhr.response);
+            } else onNotReady();
+        }).catch(()=>onNotReady());
     }
 
 
-    _mapp.getPage = function(url) {
-        const cacheKey = rewrites.cache(url);
-
-        // cache hit
-        if (pageCache[cacheKey]) return pageCache[cacheKey];
-
-        // cache miss - return promise that resolves when page is loaded.
-        return pageCache[cacheKey] = new Promise((resolve, reject)=>{
-            const page = {response: null, scriptLoadingPromise: null},
-                  onError = () => {delete pageCache[cacheKey]; reject();};
-            rq(rewrites.partialUrl(url), {})
-            .then(xhr => {
-                if (xhr.status == 200) {
-                    page.xhr = xhr;
-                    resolve(page);
-                } else onError();})
-            .catch(onError);
-        });
-    };
-
-
+    // ===========
+    //  Rendering
+    // ===========
     function renderHtml(page) {
-        _mapp.container.innerHTML = page.xhr.response;
+        container.innerHTML = page.xhr.response;
         return page;
     }
 
@@ -155,7 +121,7 @@
             // otherwise, eg. alert() would pop up over the previous page.
             setTimeout(() => {
                 [].slice.call(
-                    _mapp.container.getElementsByTagName("script")
+                    container.getElementsByTagName("script")
                 ).forEach(script => window.eval(script.textContent));
                 resolve(page);
             }, 0);
@@ -163,27 +129,57 @@
     }
 
 
-    _mapp.go = function(url) {
-        return _mapp.getPage(url)
-            .then(page=>{
-                history.pushState(null, "", rewrites.displayUrl(url));
-                return page;})
-            .then(renderHtml)
-            .then(loadScripts)
-        ;
-    };
+    // ============
+    //  Public API
+    // ============
+    function match(url) {
+        // most uses are matching the current url anyway
+        url = U(url || document.location);
+        const route = dynamicRouteFor(url);
+        return route ? route.match : null;
+    }
 
+    const pageCache = {};
+    function getPage(url) {
+        // returns a Promise
+        const key = cacheKey(url);
 
-    // Hook up event handlers to intercept navigation
-    // ----------------------------------------------
+        // cache hit
+        if (pageCache[key]) return pageCache[key];
 
+        // cache miss - return promise that resolves when page is loaded.
+        return pageCache[key] = new Promise((resolve, reject)=>{
+            const page = {response: null, scriptLoadingPromise: null},
+                onError = () => {delete pageCache[key]; reject();};
+            rq(urlOfPartial(url), {})
+                .then(xhr => {
+                    if (xhr.status == 200) {
+                        page.xhr = xhr;
+                        resolve(page);
+                    } else onError();
+                }).catch(onError);
+        });
+    }
 
+    function go(url) {
+        return getPage(url)
+        .then(page=>{history.pushState(null, "", urlForDisplay(url)); return page})
+        .then(renderHtml)
+        .then(loadScripts)
+    }
+
+    // on init hook up event handlers
+    // ------------------------------
     const which = e => {e = e || window.event; return e.which === null ? e.button: e.which};
     window.addEventListener("click", e => {
+        // skip middle/right click, click with modifiers
         if (which(e) !== 1) return;
         if (e.metaKey || e.ctrlKey || e.shiftKey) return;
         if (e.defaultPrevented) return;
 
+        // skip click handlers that aren't links,
+        // links explicitly tagged as downloads or external,
+        // and links to different origins
         let el = e.target;
         while (el && "A" !== el.nodeName) el = el.parentNode;
         if (!el || "A" !== el.nodeName) return;
@@ -191,13 +187,24 @@
         if (el.target) return;
         if (!sameOrigin(el.href)) return;
 
-        e.preventDefault();
-        _mapp.go(el.href);
+        // finally meet all the criteria to perform the rewrite
+        e.preventDefault(); go(el.href);
     }, false);
 
-
     window.onpopstate = () => {
-        _mapp.getPage(document.location.pathname).then(renderHtml);
+        getPage(document.location.pathname).then(renderHtml);
     };
-    exports.mapp = _mapp;
-}(window));
+
+
+    // expose public api
+    // -----------------
+    return {
+        get container () {return container},
+        set container (v) {container = v},
+        //container: container,
+        getPage: getPage,
+        go: go,
+        match: match,
+        ready: ready,
+    };
+})();
